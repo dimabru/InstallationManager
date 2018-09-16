990afeb0d1dd61d4dcc46c50c49c371bc661fca9
Dima Brusilovski <Dmitry.Brusilovski@allscipts.com>
2018-09-16 10:53:44 +0300

    Added notes to ProjectInfo

--- Project Creator/ProjectCreatorApplication/ProjectCreatorEngine/ProjectInfo.cs
+++ Project Creator/ProjectCreatorApplication/ProjectCreatorEngine/ProjectInfo.cs
@@ -9,6 +9,8 @@ namespace ProjectCreatorEngine
 {
     public class ProjectInfo
     {
+        // Project location needs to be extracted from an xml file. 
+        // If file corrupted or missing, go to DefaultInfo.DefaultPath.
         public static string ProjectLocation = DefaultInfo.DefaultPath;
     }
 }
