ea4744bc71bbef3068cd02f977b3c944beb5cd4d
daniel26897 <danka26897@gmail.com>
2018-10-20 21:55:40 +0300

    delete file plugin was added, currently deletes only one file

---
+++ DeleteFilePlugin/DeleteFilePlugin/DeleteFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFilePlugin
+{
+    class DeleteFile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file path: ");
+            string path = Console.ReadLine();
+            Console.WriteLine("{0}", Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            Console.WriteLine("{0}", File.Exists(path));
+            string destFile = System.IO.Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), path);
+            try
+            {
+                File.Delete(destFile);
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+
+            Console.WriteLine("{0} was deleted!",path);
+            Console.ReadKey();
+        }
+    }
+}
