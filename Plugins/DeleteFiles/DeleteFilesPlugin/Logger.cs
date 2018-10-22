e81ac64bfd2737099d53eeb4b64362f965927b03
dimabru <dimabru@gmail.com>
2018-10-22 21:38:31 +0300

    Created DeleteFiles plugin

---
+++ Plugins/DeleteFiles/DeleteFilesPlugin/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFilesPlugin
+{
+    class Logger
+    {
+        public static void LogError(string msg)
+        {
+            File.AppendAllText(".\\Error.log", msg);
+        }
+
+        public static void LogWarning(string msg)
+        {
+            File.AppendAllText(".\\Warning.log", msg);
+
+        }
+
+        public static void LogNotify(string msg)
+        {
+            File.AppendAllText(".\\Notify.log", msg);
+
+        }
+    }
+}
---
+++ Plugins/DeleteFiles/DeleteFilesPlugin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFilesPlugin
+{
+    class Program
+    {
+        // Create 3 log files.
+        // Warning.log
+        // Error.log
+        // Notify.log
+        static void Main(string[] args)
+        {
+            if (args.ToList().Count == 0)
+            {
+                Logger.LogError("No files to delete");
+                return;
+            }
+            List<string> notRemovedFiles = new List<string>();
+            foreach (string arg in args)
+            {
+                FileAttributes attr = File.GetAttributes(arg);
+                if (!attr.HasFlag(FileAttributes.Directory))
+                {
+                    Logger.LogNotify($"Deleting file {arg}");
+                    try
+                    {
+                        File.Delete(arg);
+                    }
+                    catch ()
+                    {
+                        notRemovedFiles.Add(arg);
+                        Logger.LogError($"Unable to delete file {arg} because...");
+                    }
+                    catch ()
+                    {
+                        Logger.LogError($"Unable to delete file {arg} because...");
+                    }
+                }
+
+            }
+            // If not all files were removed
+            if (notRemovedFiles.Any())
+            {
+                Logger.LogWarning("The following were not removed");
+            }
+            // else
+            Logger.LogNotify("Process completed ");
+        }
+    }
+}
